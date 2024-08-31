288a66a39e0c0c00158b5cb9298893329e54a785
Gabriel-Souza18 <gabrielsisou@gmail.com>
2024-08-31 16:10:50 -0300

    Oito Rainhas terminado

--- oito_rainhas/README.md
+++ oito_rainhas/README.md
@@ -1,10 +1,32 @@
 # Oito Rainhas
-Esse programa resolve o problema das 8 rainhas, lendo um arquivo contendo um grafo, com incompatibilidades das rainhas, e imprimindo a solucao
+Esse programa resolve o problema das 8 rainhas, lendo um arquivo contendo um grafo, com incompatibilidades das rainhas, e imprimindo a solução
 
-## Funcoes principais
-
-## Exemplo de sainda
-
+## Função principal
+```
+def oito_rainhas(grafo: nx.Graph):
+    [...]
+    
+    while len(solucao)< 8:
+        [...]
+        while nao_considerados:
+            node_selecionado = nao_considerados.pop(0)
+            ultimos_selecionados.append(node_selecionado)
+            if node_selecionado not in solucao:
+                solucao.append(node_selecionado)
+                
+                if node_selecionado in nao_considerados:
+                    nao_considerados.remove(node_selecionado)
+                    
+                for vizinho in grafo.neighbors(node_selecionado):
+                    if vizinho in nao_considerados:
+                        nao_considerados.remove(vizinho)
+                         
+        return solucao
+```
+## Exemplo de saida
+```
+['3', '9', '22', '28', '32', '47', '53', '58']
+```
 ## Para rodar 
 Tem que intalar as bibliotecas:
 * Networkx\
@@ -14,3 +36,7 @@ https://networkx.org/documentation/stable/install.html
 * Matplotlib\
 Site com tutorial para instalação:
 https://matplotlib.org/stable/users/installing/index.html
+
+### Feito por
+ Gabriel da Silva Souza<br>
+* Email - gabrielsisou@gmail.com
--- oito_rainhas/oito_rainhas.py
+++ oito_rainhas/oito_rainhas.py
@@ -2,50 +2,41 @@ import networkx as nx
 import matplotlib.pyplot as plt
     
 def plotar_grafo(grafo):
-    pos = nx.circular_layout(grafo)
-    nx.draw(grafo, pos, with_labels=True)
+    pos = nx.spring_layout(grafo)
+    nx.draw_networkx(grafo, pos, with_labels=True, arrows=True)
     plt.show()
 
 def lergrafo():
     grafo = nx.read_gml("oito_rainhas/Tabuleiro_com_incompatibilidades.gml")
     return grafo
 
-def is_safe(solucao, linha, coluna):#confere se é seguro colocar uma rainha 
-    for i in range(linha):
-        if solucao[i] == coluna or \
-           solucao[i] - i == coluna - linha or \
-           solucao[i] + i == coluna + linha:
-            return False
-    return True
 
-def solve_oito_rainhas(grafo, solucao, linha):
-    if linha == len(grafo.nodes()):#condicao de parada
-        return True
+def oito_rainhas(grafo: nx.Graph):
+    nodes = list(grafo.nodes())
+    solucao = []
+    ultimos_selecionados = []
     
-    for coluna in range(len(grafo.nodes())):
-        if is_safe(solucao, linha, coluna):
-            solucao[linha] = coluna
-            if solve_oito_rainhas(grafo, solucao, linha + 1):#funcao recursiva
-                return True
-            solucao[linha] = -1
-    return False
-
-def oito_rainhas(grafo):
-    solucao = [-1] * len(grafo.nodes())#todas os valores como -1, ou seja nenhuma rainha colocada
-    if solve_oito_rainhas(grafo, solucao, 0):
+    while len(solucao)< 8:
+        nao_considerados = nodes.copy()
+        solucao = []
+        for i in ultimos_selecionados:
+            nao_considerados.remove(i)
+        while nao_considerados:
+            node_selecionado = nao_considerados.pop(0)
+            ultimos_selecionados.append(node_selecionado)
+            if node_selecionado not in solucao:
+                solucao.append(node_selecionado)
+                
+                if node_selecionado in nao_considerados:
+                    nao_considerados.remove(node_selecionado)
+                    
+                for vizinho in grafo.neighbors(node_selecionado):
+                    if vizinho in nao_considerados:
+                        nao_considerados.remove(vizinho)
+                         
         return solucao
-    else:
-        return None
-
 def main():
     grafo = lergrafo()
-    solucao = oito_rainhas(grafo)
-    if solucao:
-        print("Solução encontrada:")
-        for linha, coluna in enumerate(solucao):
-            print(f"Rainha na linha {linha}, coluna {coluna}")
-    else:
-        print("Nenhuma solução encontrada")
-
+    print(oito_rainhas(grafo))
 if __name__ == "__main__":
     main()
